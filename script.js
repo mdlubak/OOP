d57e6bea6c52e3b2847cf15d7c9aa05bbaf0fea5
Martyna <mdlubak@gmail.com>
2017-01-25 03:37:02 +0000

    add warranty

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var GalaxyS6 = new Telefon('Samsung', 2200, 'czarny');
-var iPhone6S = new Telefon('Apple', 3300, 'srebrny');
-var OnePlusOne = new Telefon('OnePlus', 1100, 'bialy');
+var GalaxyS6 = new Telefon('Samsung', 2200, 'czarny','', '10', '');
+var iPhone6S = new Telefon('Apple', 3300, 'srebrny','USD', '10', '');
+var OnePlusOne = new Telefon('OnePlus', 1100, 'bialy','EUR', '20', '');
 
-function Telefon(marka, cena, kolor) {
-	this.marka = marka;
-	this.cena = cena + ' PLN';
-	this.kolor = kolor;
+function Telefon(brand, price, color, currency, warrantyPercent, warrantyCost) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.currency = currency || 'PLN';
+	this.warrantyCost = getWarrantyCost(price, warrantyPercent);
+	this.warrantyPercent = warrantyPercent;
+}
+
+function getWarrantyCost(price, warrantyPercent) {	
+	return (warrantyPercent/100) * price;
 }
 
 Telefon.prototype.printInfo = function() {
-	console.log('Marka telefonu to ' + this.marka + ", kolor to " + this.kolor + ', a cena to ' + this.cena + '.');
+	console.log('Marka telefonu to ' + this.brand + ", kolor to " + this.color + ', a cena to ' + this.price + this.currency +'.' + ' Przedłużona gwarancja: ' + this.warrantyCost );
 }
 
 GalaxyS6.printInfo();
